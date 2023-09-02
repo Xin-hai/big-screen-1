56fe641f76355223ed949f3a849d3757d73eb243
Xin-hai <aa1940358283@163.com>
2023-09-02 23:57:21 +0800

    添加 axios响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,13 +5,24 @@ const service = axios.create({
     timeout: 5000
 })
 
-// 拦截器
+// 请求拦截器
 service.interceptors.request.use(
     (config) => {
         config.headers.icode = 'input you icode'
         return config // 必须返回配置
     },
-    (error) => {return Promise.reject(error)}
+    (error) => {
+        return Promise.reject(error)
+    }
 )
+// 响应拦截器确保不出现 response.data.data
+service.interceptors.response.use((response) => {
+    const { success, message, data } = response.data
+    if (success) {
+        return data
+    } else {
+        return Promise.reject(new Error(message))
+    }
+})
 
 export default service
