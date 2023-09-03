8611738594c9950439a035df54742b513d5b31ac
Xin-hai <aa1940358283@163.com>
2023-09-03 13:50:58 +0800

    初始化雷达图

--- src/components/RadarBar.vue
+++ src/components/RadarBar.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-    雷达图
+    <div>【云端报警风险】</div>
+    <div ref="target" class="w-full h-full"></div>
   </div>
 </template>
 
 <script setup>
+import * as echarts from 'echarts';
+import {onMounted, ref, watch} from "vue";
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
 
+let myChart = null
+const target = ref(null)
+
+onMounted(()=> {
+  myChart = echarts.init(target.value)
+  renderChart()
+})
+
+const renderChart = () => {
+  const options = {
+
+  }
+  myChart.setOption(options)
+}
+
+watch(()=>props.data,renderChart)
 </script>
 
 <style scoped lang="scss">
