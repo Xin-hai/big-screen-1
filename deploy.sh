502c255dc12968e23554247b3c3fc247586fa373
Xin-hai <aa1940358283@163.com>
2023-09-05 01:23:40 +0800

    优化 准备部署

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到github 此两行为注释
-# git push -f git@github.com:Xin-hai/big-screen-1.git master:gh-pages
+ git push -f git@github.com:Xin-hai/big-screen-1.git master:gh-pages
 
 # 部署到gitee 下面这行正常运行
-git push -f git@gitee.com:ZXInhai/big-screen-1.git master:gh-pages
+#git push -f git@gitee.com:ZXInhai/big-screen-1.git master:gh-pages
 
 # 回到之前的那一层目录
 cd -
